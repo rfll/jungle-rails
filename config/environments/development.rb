c966115f6a93cda942e459286f62294b7b59ac8c
Ryan Fortin <ryanwfortin@gmail.com>
2022-09-21 21:45:09 +0000

    fix price rendering inconsistencies

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -70,7 +70,6 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   # config.file_watcher = ActiveSupport::EventedFileUpdateChecker
-
   config.file_watcher = ActiveSupport::FileUpdateChecker
 
   # Uncomment if you wish to allow Action Cable access from any origin.
